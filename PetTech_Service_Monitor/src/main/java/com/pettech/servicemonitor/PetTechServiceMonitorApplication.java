5a088b12594f1aae2bbb787a7245072dd2851f83
SolomonRojer <107550419+SolomonRojer@users.noreply.github.com>
2022-07-22 11:57:55 +0530

    Add files via upload

---
+++ PetTech_Service_Monitor/src/main/java/com/pettech/servicemonitor/PetTechServiceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.pettech.servicemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTechServiceMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechServiceMonitorApplication.class, args);
+	}
+
+}
---
+++ PetTech_Service_Monitor/src/test/java/com/pettech/servicemonitor/PetTechServiceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pettech.servicemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTechServiceMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
