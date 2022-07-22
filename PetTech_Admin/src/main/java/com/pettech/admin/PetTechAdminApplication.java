a1b09d47877c8665d4c669250bae6dbe48aa5cb5
SolomonRojer <107550419+SolomonRojer@users.noreply.github.com>
2022-07-22 11:55:02 +0530

    Add files via upload

---
+++ PetTech_Admin/src/main/java/com/pettech/admin/PetTechAdminApplication.java
@@ -0,0 +1,13 @@
+package com.pettech.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTechAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechAdminApplication.class, args);
+	}
+
+}
---
+++ PetTech_Admin/src/test/java/com/pettech/admin/PetTechAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pettech.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTechAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
